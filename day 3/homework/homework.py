c05126c471244a6f2ac01547003538e276cbccf4
MefeToma <tomasepashvili@gmail.com>
2024-07-21 23:54:20 +0400

    first commit

---
+++ day 3/homework/homework.py
@@ -0,0 +1,20 @@
+# 1) მომხმარებელს input-ის გამოყენებით შემოატანინეთ სახელი, და მომხმარებელი როცა შემოიყვანს თავის სახელს უნდა გამოიტანოს hello მომხმარებელის_სახელი
+
+name = input("Enter your name: ")
+
+print("Hello " + name)
+
+
+# 2) მომხმარებელს input-ის გამოყენებით შემოატანინეთ სახელი, გვარი, ასაკი და ქალაქი. როცა  შემოიტანს მომხმარებელი ყველაფერს მერე უნდა დაუპრინტოთ გრძელი წინადადება
+
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+city = input("enter your city: ")
+color = input("enter your favourite color: ")
+food = input("enter your favourite food: ")
+
+print("gamarjoba, chemi saxeli da gvaria " + name + " " + surname + ", adgili sadac me vimyofebi aris " + city + ". chemi sayvareli feria " + color + ", chemi sayvareli sawmelia " + food)
+
+# 3) კომენტარებით ახსენით რა არის input-ი და output-ი, რამდენიმე მაგალითიც გააკეთეთ input-ებზე
+
+# Input ნიშნავს ინფორმაციის შეყვანას, Outputს კიდე ეკრანზე ინფორმაცია გამოაქვს, მაგალითად: კლავიატურა არის input device ხოლო თვითონ მონიტორი სადაც კლავიატურაზე დაწერილი ინფორმაცია აისახება არის output device
---
+++ day 7/homework/homework.py
@@ -0,0 +1,4 @@
+name = input("hello, what is your name?: ")
+city = input("nice to meet you, what is your city: ")
+color = input("what is your favourite color: ")
+food = input("mine too, what is your favourite food: ")
